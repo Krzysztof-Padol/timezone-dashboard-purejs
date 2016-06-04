e02aadfc2afb716ee001809791ca3d9d9a011320
Krzysztof Padol <krzysztof.padol@pearson.com>
2016-06-05 00:31:51 +0200

    Added new-timezone-form structure

---
+++ src/dashboard/new-timezone-form/new-timezone-form.js
@@ -0,0 +1,22 @@
+import template from './new-timezone-form.html';
+import doT from 'dot';
+
+export const exceptionMsg = {
+}
+
+export const elementsQuery = {
+};
+
+export class Dashboard {
+  constructor(config = {}) {
+  }
+
+  checkConfig() {
+  }
+
+  render() {
+  }
+
+  assignDomElements() {
+  }
+}
